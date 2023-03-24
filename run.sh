49f8bc1cdf1387da4f23161cde7dc5728d88daab
Monnawat Tongkaw <monnawat.ton@student.mahidol.edu>
2023-03-24 22:07:13 +0700

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-gunicorn -w 2 -k uvicorn.workers.UvicornWorker app:app
+export MONGODB_URL="mongodb://modemss:opH9opk5PIFjYVlzJ3JsnslgzvywXrUsDOqo0ZgTDced6DoFHLqJ0XRdutfjXqTKx66qrRFTVQw9ACDbdmau5w==@modemss.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@modemss@"
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker app:app
